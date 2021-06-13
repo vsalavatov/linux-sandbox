dbdbceb0a9579d48c799183ec80a245293e13db9
Anton Shangareev <shananton2000@gmail.com>
2021-06-13 23:12:43 +0300

    Add niceness test

--- tests.py
+++ tests.py
@@ -15,6 +15,12 @@ class TestSandbox(unittest.TestCase):
             output, stderr = proc.communicate()
             return output.decode("utf-8"), stderr.decode("utf-8")
 
+    @staticmethod
+    def run_sandbox(options, executable, args):
+        cmd = f'{sandbox_executable} {common_options} {options} -- {executable} {args}'
+        return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+
+
     def test_simple(self):
         executable = './build/examples/echo42/echo42'
         output, _ = self.get_sandbox_output('', executable, '')
@@ -71,6 +77,15 @@ class TestSandbox(unittest.TestCase):
         executable = './build/examples/killparent/killparent'
         output, stderr = self.get_sandbox_output('', executable, '')
         self.assertEqual('Failed to kill watcher.', output.strip())
+
+    def test_niceness(self):
+        executable = './build/examples/sleep30/sleep30'
+        for expected_ni in [-7, 11]:
+            with self.run_sandbox(f'-n {expected_ni} -t 1', executable, ''):
+                with os.popen("sh -c 'ps -p $(pidof sleep30) -o ni='") as proc:
+                    niceness = int(proc.read())
+                self.assertEqual(niceness, expected_ni)
+
     
 if __name__ == '__main__':
     current_directory = os.getcwd()
